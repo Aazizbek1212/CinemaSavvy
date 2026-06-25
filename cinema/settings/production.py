d876730c1abf35116fb234c4f1fbf6a7c5133eb4
Ubuntu <ubuntu@ip-172-26-4-72.eu-central-1.compute.internal>
2026-06-25 05:44:07 +0000

    feat: YouTube embed, AWS Lightsail deploy fixes

--- cinema/settings/production.py
+++ cinema/settings/production.py
@@ -1,19 +1,10 @@
 from .base import *  # noqa
+from decouple import config
 
-DEBUG = False
-
-# ALLOWED_HOSTS, SECRET_KEY va boshqalar .env / muhit o'zgaruvchilaridan keladi (base.py)
-
-# Xavfsizlik (HTTPS ortida ishlaganda)
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", default="True") == "True"
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-
-EMAIL_BACKEND = config(
-    "EMAIL_BACKEND",
-    default="django.core.mail.backends.smtp.EmailBackend",
-)
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_HSTS_SECONDS = 0
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_PRELOAD = False
