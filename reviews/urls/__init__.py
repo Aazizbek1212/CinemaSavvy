105747964a06b013d67030f48d4dcc2050f0459b
Azizbek <azizbekilyosov1212@gmail.com>
2026-07-06 13:57:31 +0500

    ''

--- reviews/urls/__init__.py
+++ reviews/urls/__init__.py
@@ -1,5 +1,5 @@
-from .movie_review_views import MovieReviewListView, MovieReviewCreateView
-from .review_detail_views import ReviewDetailView
+from reviews.views.movie_review_views import MovieReviewListView, MovieReviewCreateView
+from reviews.views.review_detail_views import ReviewDetailView
 
 __all__ = [
     'MovieReviewListView',
