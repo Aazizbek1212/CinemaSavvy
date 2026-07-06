ab24f1419910c32e9e87eac65c1e0a2413dc89c2
Azizbek <azizbekilyosov1212@gmail.com>
2026-07-06 13:41:00 +0500

    ''

--- watchlist/serializers.py
+++ watchlist/serializers.py
@@ -1,36 +1,39 @@
 from rest_framework import serializers
 from .models import Watchlist
 
+
 class WatchlistSerializer(serializers.ModelSerializer):
-
     movie_title = serializers.CharField(
-        source="movie.title",
+        source='movie.title',
         read_only=True
     )
-
     movie_slug = serializers.CharField(
-        source="movie.slug",
+        source='movie.slug',
         read_only=True
     )
-
     movie_poster = serializers.ImageField(
-        source="movie.poster",
+        source='movie.poster',
+        read_only=True
+    )
+    movie_year = serializers.IntegerField(
+        source='movie.year',
+        read_only=True
+    )
+    movie_rating = serializers.FloatField(
+        source='movie.rating',
         read_only=True
     )
 
     class Meta:
         model = Watchlist
-
-        fields = [
-            "id",
-            "movie",
-            "movie_title",
-            "movie_slug",
-            "movie_poster",
-            "created_at",
-        ]
-
-        read_only_fields = [
-            "id",
-            "created_at",
-        ]
+        fields = (
+            'id',
+            'movie',
+            'movie_title',
+            'movie_slug',
+            'movie_poster',
+            'movie_year',
+            'movie_rating',
+            'created_at',
+        )
+        read_only_fields = ('id', 'created_at')
--- watchlist/views.py
+++ watchlist/views.py
@@ -1,21 +1,54 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.authentication import SessionAuthentication
 
 from .models import Watchlist
 from .serializers import WatchlistSerializer
 
-class WatchlistViewSet(viewsets.ModelViewSet):
 
+class WatchlistViewSet(viewsets.ModelViewSet):
     serializer_class = WatchlistSerializer
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return (
-            Watchlist.objects
-            .select_related("movie")
-            .filter(user=self.request.user)
-        )
+        # Faqat o'z foydalanuvchisining watchlistini qaytaradi
+        return Watchlist.objects.filter(
+            user=self.request.user
+        ).select_related('movie').order_by('-created_at')
 
     def perform_create(self, serializer):
+        # Yangi item qo'shilganda user avtomatik biriktiriladi
         serializer.save(user=self.request.user)
 
+    @action(detail=False, methods=['post'], url_path='toggle')
+    def toggle(self, request):
+        """Kino watchlistda bor bo'lsa o'chiradi, yo'q bo'lsa qo'shadi"""
+        movie_id = request.data.get('movie_id')
+        if not movie_id:
+            return Response(
+                {'error': 'movie_id majburiy'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        item = Watchlist.objects.filter(
+            user=request.user,
+            movie_id=movie_id
+        ).first()
+
+        if item:
+            item.delete()
+            return Response({'status': 'removed'}, status=status.HTTP_200_OK)
+        else:
+            obj = Watchlist.objects.create(
+                user=request.user,
+                movie_id=movie_id
+            )
+            serializer = self.get_serializer(obj)
+            return Response(
+                {'status': 'added', 'item': serializer.data},
+                status=status.HTTP_201_CREATED
+            )
