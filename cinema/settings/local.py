7ffdcfb821de8de7ab2e4bfdb04641fe7ff3d72f
Azizbek <azizbekilyosov1212@gmail.com>
2026-09-16 09:13:54 +0500

    ''

--- cinema/settings/local.py
+++ cinema/settings/local.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "db", "3.125.45.192.sslip.io"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
